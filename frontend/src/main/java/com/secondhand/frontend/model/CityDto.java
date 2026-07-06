ffb72215b799900b466576f850ee6dfdff9f2ca4
Ensie <Ensiebahrevar029@gmail.com>
2026-07-06 03:41:45 +0330

    adding front

---
+++ frontend/src/main/java/com/secondhand/frontend/model/CityDto.java
@@ -0,0 +1,27 @@
+package com.secondhand.frontend.model;
+
+public class CityDto {
+
+    private Long id;
+
+    private String name;
+
+    public CityDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
